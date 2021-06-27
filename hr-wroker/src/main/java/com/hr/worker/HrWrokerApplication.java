0a23f1749413d522a850975f7a021185037e3cdb
Jean Lacerda <jean.sena@sicoob.com.br>
2021-06-27 12:26:27 -0300

    Comitando projeto HR work

---
+++ hr-wroker/src/main/java/com/hr/worker/HrWrokerApplication.java
@@ -0,0 +1,13 @@
+package com.hr.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWrokerApplication.class, args);
+	}
+
+}
