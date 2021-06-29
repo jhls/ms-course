46651eba2e44a23f99f974e394a239479d1a3cb0
Jean Lacerda <jean.sena@sicoob.com.br>
2021-06-28 21:05:22 -0300

    Primeiro endpoint de trabalhadores

--- hr-worker/src/main/java/com/hr/worker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/hr/worker/HrWorkerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HrWrokerApplication {
+public class HrWorkerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HrWrokerApplication.class, args);
+		SpringApplication.run(HrWorkerApplication.class, args);
 	}
 
 }
