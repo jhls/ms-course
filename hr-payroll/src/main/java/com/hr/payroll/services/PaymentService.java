d4d51489bbde1c916caf84cef40cf5757a779e7b
Jean Lacerda <jean.sena@sicoob.com.br>
2021-06-28 21:39:12 -0300

    restTemplate

--- hr-payroll/src/main/java/com/hr/payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/hr/payroll/services/PaymentService.java
@@ -1,14 +1,33 @@
 package com.hr.payroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.hr.payroll.entities.Payment;
+import com.hr.payroll.entities.Worker;
 
 @Service
 public class PaymentService {
 	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	public Payment getPayment(Long workerId, Integer days) {
-		return new Payment("Bob",200.0,days);
+		
+		Map<String, String> uriVariables = new HashMap<>();
+		
+		uriVariables.put("id", workerId.toString());
+		
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(),worker.getDailyIncome(),days);
 	}
 }
